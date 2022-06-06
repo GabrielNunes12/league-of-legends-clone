f55b6e1916f223a4adb59a37ac564dcefc9654e4
Gabriel <prismaticnetwork12@gmail.com>
2022-06-06 10:32:31 -0300

    feat: first commit

--- league-clone/src/App.tsx
+++ league-clone/src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Container, Image, ImageContainer, LoginWrapper, LoginButton} from './styles';
 
 function App() {
+  const [userName, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <LoginWrapper>
+          <small>Username:</small>
+          <input type="text" placeholder='Type your username' onChange={(event) => setUsername(event.target.value)}/>
+          <small>Password:</small>
+          <input type="password" placeholder='Type your password' onChange={(event) => setPassword(event.target.value)}/>
+          {userName && password !== '' ? <LoginButton isActive={true}>Login</LoginButton> : <LoginButton isActive={false}>Login</LoginButton>}
+        </LoginWrapper>
+        <ImageContainer>
+          <Image background={true}/>
+        </ImageContainer>
+      </Container>
     </div>
   );
 }
---
+++ league-clone/src/styles.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+let logo = require('./images/background_lol.jpg')
+
+type ImageType = {
+  background: boolean
+}
+type ButtonTypes = {
+  isActive: boolean
+}
+
+export const Container = styled.div`
+  width: 98vw;
+  height: 95vh;
+  display: flex;
+  background-color: white;
+  @media(max-width: 1024px) {
+    width: 50%;
+    height: 50%;
+    background-color: white;
+  }
+`
+export const ImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+export const Image = styled.div`
+  width: 1500px;
+  height: 900px;
+  margin-left: 32em;
+  @media(max-width: 1024px) {
+    margin-left: 1.5em;
+    width: 750px;
+    height: 750px;
+  }
+  background: url("${(props:ImageType) => props.background ? logo : ''}") no-repeat;
+`
+export const LoginWrapper = styled.div`
+  input {
+    justify-content: space-around;
+    padding: 10px;
+  }
+`
+export const LoginButton = styled.button`
+  padding: 10px;
+  justify-content: center;
+  background: ${(props: ButtonTypes) => props.isActive ? 'palevioletred' : 'white'};
+  font-color: 'palevioletred';
+  font-size: 1em;
+  cursor: ${(props: ButtonTypes) => props.isActive ? 'pointer': 'not-allowed'};
+  opacity: ${(props: ButtonTypes) => props.isActive ? '' : '0.6'};
+  border: ${(props: ButtonTypes) => props.isActive ? 'none' : '2px solid palevioletred'};
+`;
